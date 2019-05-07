47067b9806124cb7e18b65d589bc3e74eb81da54
Octagon <42719041+MyOctagon@users.noreply.github.com>
2019-05-07 17:05:56 +0600

    Update1

--- bot.py
+++ bot.py
@@ -1,25 +1,59 @@
 import discord
-
 from discord.ext import commands
 
-import asyncio
-
-bot=commands.Bot(command_prefix='D')
-
-
+bot = commands.Bot(command_prefix='$')
 
 @bot.event
-
 async def on_ready():
-    
-    print('The bot is ready!')
-    
-    print(bot.user.name)
+print('Logged in as')
+print(bot.user.name)
+print(bot.user.id)
+print('------')
 
-    print(bot.user.id)
+@bot.command()
+async def add(ctx, a: int, b: int):
+await ctx.send(a+b)
 
+@bot.command()
+async def multiply(ctx, a: int, b: int):
+await ctx.send(a*b)
 
+@bot.command()
+async def greet(ctx):
+await ctx.send(":smiley: :wave: Hello, there!")
 
+@bot.command()
+async def cat(ctx):
+await ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
+
+@bot.command()
+async def info(ctx):
+embed = discord.Embed(title="Trash bot", description="Most idiot bot ever.", color=0xeee647)
+
+# give info about you here
+embed.add_field(name="A smart guy", value="TiCkShot#9401")
+
+# Shows the number of servers the bot is member of.
+embed.add_field(name="Server count", value=f"{len(bot.guilds)}")
+
+# give users a link to invite thsi bot to their server
+embed.add_field(name="Invite", value="[Invite link](https://discordapp.com/api/oauth2/authorize?client_id=573909670947717133&permissions=8&scope=bot)")
+
+await ctx.send(embed=embed)
+
+bot.remove_command('help')
+
+@bot.command()
+async def help(ctx):
+embed = discord.Embed(title="Trash bot", description="A Very idiot bot. List of commands are:", color=0xeee657)
+
+embed.add_field(name="$add X Y", value="Gives the addition of **X** and **Y**", inline=False)
+embed.add_field(name="$multiply X Y", value="Gives the multiplication of **X** and **Y**", inline=False)
+embed.add_field(name="$greet", value="Gives a nice greet message", inline=False)
+embed.add_field(name="$cat", value="Gives a cute cat gif to lighten up the mood.", inline=False)
+embed.add_field(name="$info", value="Gives a little info about the bot", inline=False)
+embed.add_field(name="$help", value="Gives this message", inline=False)
+
+await ctx.send(embed=embed)
 
 bot.run('NTczOTA5NjcwOTQ3NzE3MTMz.XMx4Fw.CxJQG3DhentTzQQXGX3LLR5D2LQ')
-
